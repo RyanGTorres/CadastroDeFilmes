cc7eb709847e9149fa890b27423fae4f8fcb9c0f
RyanGTorres <ryryan009@gmail.com>
2025-02-18 23:14:01 -0300

    feat: Criação de classes novas, NinjaRepository, NinjaService e a interface MissoesRepository

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class NinjaController {
 
-
     @GetMapping("/boasvindas")
     public String boasvindas(){
         return "Essa é minha primeira mensagem nessa rota";
