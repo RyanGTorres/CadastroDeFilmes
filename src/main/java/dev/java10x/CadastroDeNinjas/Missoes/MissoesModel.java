79ea40d7fc9029a29f6c0b33cde0f670a980e19b
RyanGTorres <ryryan009@gmail.com>
2025-02-21 23:20:41 -0300

    fix: inclui a anotation @JsonIgnore para evitar o erro de loop

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,6 +1,7 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -23,6 +24,7 @@ public class MissoesModel {
     private char rankMissao;
     // uma missao pode ter varios ninjas @OneToMany
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninja;
 
 }
