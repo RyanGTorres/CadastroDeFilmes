be1c12a4a5aea6e7feb496379d20845ae1e22844
RyanGTorres <ryryan009@gmail.com>
2025-03-10 20:48:31 -0300

    doc: Adicina Swagger para documentar o MissoesController

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,6 +1,10 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +23,16 @@ public class MissoesController {
 
     //GET -- Mandar um requisição para mostrar missoes
     @GetMapping("/listar/{id}")
-    public ResponseEntity<?> listarNinjasPorId(@PathVariable Long id){
+
+    @Operation(summary = "Lista missoes por id", description = "Rota uma missao pelo seu id")
+
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "Missão encontrada com sucesso!"),
+            @ApiResponse(responseCode = "404", description = "Erro encontrar missao")
+    })
+    public ResponseEntity<?> listarNinjasPorId(
+            @Parameter(description = "Usuario passa um id para lista a parti dele")
+            @PathVariable Long id){
         MissoesModel missoesModel = missoesService.listarNinjasPorId(id);
 
         if (missoesModel != null){
@@ -32,6 +45,12 @@ public class MissoesController {
 
     //POST -- Mandar um requisição para criar missoes
     @PostMapping("/criar")
+    @Operation(summary = "Cria uma nova missão", description = "Cria uma nova missao no banco de dados")
+
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "201", description = "Missao criada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Erro na criação da missao")
+    })
     public ResponseEntity<String> criarMissoes(MissoesModel missoes){
         MissoesModel missoesModel = missoesService.criarMissoes(missoes);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -46,7 +65,14 @@ public class MissoesController {
 
     //DELETE -- Mandar um requisição para deletar missoes
     @DeleteMapping("/deletar/{id}")
+
+    @Operation(summary = "Deleta missao por id", description = "Rota a missao que sera excluida passando o id")
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "Missao deletada com sucesso!"),
+            @ApiResponse(responseCode = "404", description = "Erro ao deletar missao")
+    })
     public void deletarMissoes(Long id){
         missoesService.deletarMissoes(id);
     }
 }
+
